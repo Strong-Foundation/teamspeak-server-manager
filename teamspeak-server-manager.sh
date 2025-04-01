e10f6d33825e907030e2c877a2f158cdcd9f82e3
Strong-Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-04-01 15:43:20 -0400

    Create teamspeak-server-manager.sh

---
+++ teamspeak-server-manager.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Define a function to check if the script is being run with root privileges
+function check_root() {
+  # Compare the user ID of the current user to 0, which is the ID for root
+  if [ "$(id -u)" -ne 0 ]; then
+    # If the user ID is not 0 (i.e., not root), print an error message
+    echo "Error: This script must be run as root."
+    # Exit the script with a status code of 1, indicating an error
+    exit 1 # Exit the script with an error code.
+  fi
+}
+
+# Call the check_root function to verify that the script is executed with root privileges
+check_root
+
+# Define a function to gather and store system-related information
+function system_information() {
+  # Check if the /etc/os-release file exists, which contains information about the OS
+  if [ -f /etc/os-release ]; then
+    # If the /etc/os-release file is present, source it to load system details into environment variables
+    # shellcheck source=/dev/null  # Instructs shellcheck to ignore warnings about sourcing files
+    source /etc/os-release
+    # Set the CURRENT_DISTRO variable to the system's distribution ID (e.g., 'ubuntu', 'debian')
+    CURRENT_DISTRO=${ID}
+    # Set the CURRENT_DISTRO_VERSION variable to the system's version ID (e.g., '20.04' for Ubuntu 20.04)
+    CURRENT_DISTRO_VERSION=${VERSION_ID}
+    # Extract the major version of the system by splitting the version string at the dot (.) and keeping the first field
+    # For example, for '20.04', it will set CURRENT_DISTRO_MAJOR_VERSION to '20'
+    CURRENT_DISTRO_MAJOR_VERSION=$(echo "${CURRENT_DISTRO_VERSION}" | cut -d"." -f1)
+  fi
+}
+
+# Call the system_information function to gather the system details
+system_information
+
+# The following function checks if the current init system is one of the allowed options.
+function check_current_init_system() {
+  # Get the current init system by checking the process name of PID 1.
+  CURRENT_INIT_SYSTEM=$(ps -p 1 -o comm --no-headers) # Extract only the command name without the full path.
+  # CURRENT_INIT_SYSTEM=$(ps -p 1 -o comm= | awk -F'/' '{print $NF}') # Old methord to extract the command name.
+  # Convert to lowercase to make the comparison case-insensitive.
+  CURRENT_INIT_SYSTEM=$(echo "$CURRENT_INIT_SYSTEM" | tr '[:upper:]' '[:lower:]')
+  # Log the detected init system (optional for debugging purposes).
+  echo "Detected init system: ${CURRENT_INIT_SYSTEM}"
+  # Define a list of allowed init systems (case-insensitive).
+  ALLOWED_INIT_SYSTEMS=("systemd" "sysvinit" "init" "upstart" "bash" "sh")
+  # Check if the current init system is in the list of allowed init systems
+  if [[ ! "${ALLOWED_INIT_SYSTEMS[*]}" =~ ${CURRENT_INIT_SYSTEM} ]]; then
+    # If the init system is not allowed, display an error message and exit with an error code.
+    echo "Error: The '${CURRENT_INIT_SYSTEM}' initialization system is not supported. Please stay tuned for future updates."
+    exit 1 # Exit the script with an error code.
+  fi
+}
+
+# The check_current_init_system function is being called.
+check_current_init_system
+
+# The following function checks if there's enough disk space to proceed with the installation.
+function check_disk_space() {
+  # This function checks if there is more than 1 GB of free space on the drive.
+  FREE_SPACE_ON_DRIVE_IN_MB=$(df -m / | tr -s " " | tail -n1 | cut -d" " -f4)
+  # This line calculates the available free space on the root partition in MB.
+  if [ "${FREE_SPACE_ON_DRIVE_IN_MB}" -le 1024 ]; then
+    # If the available free space is less than or equal to 1024 MB (1 GB), display an error message and exit.
+    echo "Error: You need more than 1 GB of free space to install everything. Please free up some space and try again."
+    exit 1 # Exit the script with an error code.
+  fi
+}
+
+# Calls the check_disk_space function.
+check_disk_space
+
+# This is a Bash function named "get_network_information" that retrieves network information.
+function get_network_information() {
+  # This variable will store the IPv4 address of the default network interface by querying the "ipengine" API using "curl" command and extracting it using "jq" command.
+  DEFAULT_INTERFACE_IPV4="$(curl --ipv4 --connect-timeout 5 --tlsv1.2 --silent 'https://checkip.amazonaws.com')"
+  # If the IPv4 address is empty, try getting it from another API.
+  if [ -z "${DEFAULT_INTERFACE_IPV4}" ]; then
+    DEFAULT_INTERFACE_IPV4="$(curl --ipv4 --connect-timeout 5 --tlsv1.3 --silent 'https://icanhazip.com')"
+  fi
+  # This variable will store the IPv6 address of the default network interface by querying the "ipengine" API using "curl" command and extracting it using "jq" command.
+  DEFAULT_INTERFACE_IPV6="$(curl --ipv6 --connect-timeout 5 --tlsv1.3 --silent 'https://ifconfig.co')"
+  # If the IPv6 address is empty, try getting it from another API.
+  if [ -z "${DEFAULT_INTERFACE_IPV6}" ]; then
+    DEFAULT_INTERFACE_IPV6="$(curl --ipv6 --connect-timeout 5 --tlsv1.3 --silent 'https://icanhazip.com')"
+  fi
+}
